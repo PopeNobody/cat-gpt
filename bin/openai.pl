dc5cc8a2c5b4f63c01c595aaf128682548d54a7f
nobody <github2@invisiblehand.church>
2025-08-15 21:55:34 -0400

    x

--- bin/openai.pl
+++ bin/openai.pl
@@ -11,7 +11,12 @@ use Nobody::Util;
 use Nobody::PP;
 use JSON::MaybeXS();
 use OpenAI::API;
-
+BEGIN {
+  system("mkdir -p log");
+  open(STDERR,"|tee log/openai.".(serdate).".log");
+  select(STDERR); $|++;
+  select(STDOUT); $|++;
+};
 our(@VERSION) = qw( 0 1 0 );
 sub encode_json {
   JSON::MaybeXS->new({pretty=>1, utf8=>1})->encode(@_);;
@@ -21,6 +26,7 @@ sub decode_json {
 };
 # uses OPENAI_API_{KEY,URL} environment variables
 my $openai = OpenAI::API->new();    
+ddx( $openai->{config} );
 my ($file)=path("conv/current.json");
 unless($file->exists) {
   my ($dated)=path(join(".",$file, serdate));
@@ -35,14 +41,15 @@ if(defined($ENV{DRY_RUN})){
   push(@messages,{ role=>"nobody", content=>serdate() });
 } else {
   my $res = $openai->chat(
-    messages => \@messages,
+    "messages" => \@messages,
+    "model" => $ENV{OPENAI_API_MOD},
   );
   my $message = $res->{choices}[0]{message};
   push(@messages,$message);
 };
 my ($dated)=path(join(".",$file, serdate));
 my $json=encode_json(\@messages);
-open(STDOUT,">","$dated");
+open(STDOUT,"|-","tee", $dated);
 open(STDOUT,"|jq");
 say $json;
 close(STDIN);
